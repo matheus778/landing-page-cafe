3d8dc14c1eca0750c8c53fce5a176a0816640226
matheus778 <matheuzv777@gmail.com>
2021-05-26 01:57:34 -0300

    projeto finalizado

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let btn_menu = document.getElementById('btn-menu');
+// let menu_escondido = document.getElementById('menu-escondido')
+
+// btn_menu.addEventListener('click',()=>{
+  
+//   menu_escondido.style.display ='initial'
+//   setTimeout(()=>{
+//     menu_escondido.style.right = 0
+//   },10)
+//   menu_escondido.style.transition = '1s'
+// })
